5ff23b9d02eb1099941ddde601392472a9b2fd4f
Michael <ouatum44@gmail.com>
2025-10-08 18:42:57 -0400

    installed react-app

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ThreeValuePieChart from "./50/50-30-20";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <ThreeValuePieChart />
+    </div>
+  );
+}
+
+export default App;
