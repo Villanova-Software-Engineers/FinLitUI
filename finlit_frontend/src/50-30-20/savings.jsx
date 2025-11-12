f10b800d30e9975d9e9f7fbd52211a029605c2ea
Michael <ouatum44@gmail.com>
2025-11-12 18:39:38 -0500

    pie chart

---
+++ finlit_frontend/src/50-30-20/savings.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import BudgetRuleChartStep from './50-30-20';
+
+const SavingsPage = () => <BudgetRuleChartStep activeCategoryKey="20" />;
+export default SavingsPage;
---
+++ finlit_frontend/src/50-30-20/wants.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import BudgetRuleChartStep from './50-30-20';
+
+const WantsPage = () => <BudgetRuleChartStep activeCategoryKey="30" />;
+export default WantsPage;
