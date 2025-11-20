8fa761eb0a1a2546f8a3f959afc55d40288deb9c
hsunccc <hsunho0318@gmail.com>
2025-11-19 20:14:17 -0500

    needs vs wants module

--- finlit_frontend/src/App.tsx
+++ finlit_frontend/src/App.tsx
@@ -1,16 +1,18 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { AuthPage, DevAuthPage } from './auth'
-import FinLitApp from './pages/Home'
-import FinancialRoadmap from './pages/Roadmap'
-import NeedsPage from './50-30-20/needs'
-import WantsPage from './50-30-20/wants'
-import SavingsPage from './50-30-20/savings'
-import CalculatorPage from './50-30-20/calculator'
-import MainPage from './50-30-20/MainPage'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthPage, DevAuthPage } from "./auth";
+import FinLitApp from "./pages/Home";
+import FinancialRoadmap from "./pages/Roadmap";
+import NeedsPage from "./50-30-20/needs";
+import WantsPage from "./50-30-20/wants";
+import SavingsPage from "./50-30-20/savings";
+import CalculatorPage from "./50-30-20/calculator";
+import MainPage from "./50-30-20/MainPage";
+import NeedVsWants from "./needs-wants/nwModule";
+import TrueFalseCard from "./TrueFalse/TrueFalseCard";
+import NeedsWants from "./needs-wants/nwModule";
 
-import TrueFalseCard from './TrueFalse/TrueFalseCard'
 function Dashboard() {
-  return <FinLitApp />
+  return <FinLitApp />;
 }
 
 function App() {
@@ -26,9 +28,11 @@ function App() {
         <Route path="/wants" element={<WantsPage />} />
         <Route path="/savings" element={<SavingsPage />} />
         <Route path="/calculator" element={<CalculatorPage />} />
+        <Route path="/truefalse" element={<TrueFalseCard />} />
+        <Route path="/need" element={<NeedsWants />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
